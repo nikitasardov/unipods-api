423e757db76ea94a85e88e71e0fdcdb389636d04
nsardov <nsardov@gmail.com>
2018-07-07 23:25:34 +0400

    app.sh added

--- app.py
+++ app.py
@@ -9,17 +9,18 @@ try:
     from env_config import *
 except ImportError:
     with open('env_config.py', 'w', encoding='utf-8') as f:
-        f.write("APP_PORT = 4507\nAPP_ENV = 'dev'\nAPP_DEBUG = True")
+        f.write("APP_PORT = 4507\nAPP_ENV = 'dev'\nAPP_DEBUG = True\nAPP_DB = APP_ENV + '_unipods'")
         f.close()
 
     print('Default dev vars initialized, dev config written to env_config.py')
     APP_PORT = 4507
     APP_ENV = 'dev'
     APP_DEBUG = True
+    APP_DB = APP_ENV + '_unipods'
 
 from eve import Eve
 app = Eve()
-
+print('====> Using db "{}" <===='.format(APP_DB))
 
 @app.after_request
 def cors_headers(response):
--- settings.py
+++ settings.py
@@ -1,9 +1,21 @@
+try:
+    from env_config import *
+except ImportError:
+    with open('env_config.py', 'w', encoding='utf-8') as f:
+        f.write("APP_PORT = 4507\nAPP_ENV = 'dev'\nAPP_DEBUG = True\nAPP_DB = APP_ENV + '_unipods'")
+        f.close()
+
+    print('Default dev vars initialized, dev config written to env_config.py')
+    APP_PORT = 4507
+    APP_ENV = 'dev'
+    APP_DEBUG = True
+    APP_DB = APP_ENV + '_unipods'
+
 # замените user, password, ds049945.mongolab.com, example на ваши данные доступа к БД.
-MONGO_URI = "mongodb://127.0.0.1:27017/dev_unipods"
+MONGO_URI = "mongodb://127.0.0.1:27017/" + APP_DB
 
 # По умолчанию Eve запускает API в режиме "read-only" (т.е. поддерживаются только GET запросы),
 # мы включаем поддержку методов POST, PUT, PATCH, DELETE.
-#RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 RESOURCE_METHODS = ['GET', 'POST']
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
